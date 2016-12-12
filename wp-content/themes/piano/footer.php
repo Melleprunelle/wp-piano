7696533da0a1bf73059c3a3d0f244c38a0e2e2d0
Megaju <julien_malle@outlook.fr>
2016-12-12 17:47:43 +0100

    commit avant pull

--- wp-content/themes/piano/footer.php
+++ wp-content/themes/piano/footer.php
@@ -2,6 +2,6 @@
 <footer id="footer">
     <p> Copyright &#169;
         <?php print(date(Y)); ?>
-          Julien Malle et Lise Poirier</p>
-          <p>Blog propulsé par <a href="http://wordpress.org/">WordPress</a> et con&ccedil;u par <a href="#">Julien Malle et Lise Poirier</a></p>
+          Julien Malle et Lise Poirier
+    </p>
 </footer>
